0cf1cdd60a8e3686911683a715041d0d042c38eb
Kaltun Hassan <hassank@augsburg.edu>
2022-10-01 13:38:22 -0500

    added comments on test-app.js

--- test.-app.js
+++ test.-app.js
@@ -1,32 +1,42 @@
 import test from "node:test";
 import { agent, splitter, target } from "./app.js" ;
  
+//How I would test if the app was able to run
+
 //AGENT
+
+
+//Test for data agent is sending off to splitter
 describe('agent function', () => {
-    const testAgentDir = 'agent/';
+    const testAgentDir = 'agent/'; //agent folder
     const testInvalidAngentDir = 23235325;
     const app = require("./index");
- 
+    //call agent function, and give it input, data agent is sending to splitter stored in agentFnResult
+    const agentFnResult = agent(testAgentDir);
+    //expect to exist
+    expect(agentFnResult).toExist();
+
+    //test case: testing if input is null value
     test('is null', () => {
         const agentFnResult = agent(testAgentDir);
         expect(agentFnResult).toBeNull();
     });
-   
+   //test case: testing if input is valid/exists
     test('returns success value', () => {
         const agentFnResult = agent(testAgentDir);
         expect(agentFnResult).toExist();
     });
- 
+    //test case: retuns error bc of invalid input(constIvalidAgenDir)
     test('returns error due to invalid input', () => {
         const agentFnResult = agent(testInvalidAngentDir);
         expect(agentFnResult).toBeTruthy();
-   
+   //test case: retuns error bc of falsey input
     test('returns error due to fasley input', () => {
             const agentFnResult = agent(testInvalidAngentDir);
             expect(agentFnResult).toBeFalsy();
  
     });
- 
+    //test case: returns server/port agent begins on
     test('returns server staring on port 9997',() => {
         const agentFnResult = agent(testAgentDir);
     expectapp.listen(3000, () => console.log("server starting on port ___!"));
@@ -36,31 +46,41 @@ describe('agent function', () => {
  
  
   //SPLITTER
+  //Test for data splitter is sending off to target
   describe('splitter function', () => {
-    const testSplittertDir = 'splitter/';
+    const testSplittertDir = 'splitter/';//splitter folder
     const testInvalidSplitterDir = 66666;
     const app = require("./index");
- 
+    
+    //call splitter function, and give it input, data splitter is sending to target stored in agentFnResult
+    const agentFnResult = agent(testAgentDir);
+    //expect to exist
+    expect(agentFnResult).toExist();
+
+    //test case: testing if input is null value
     test('is null', () => {
         const splitterFnResult = splitter(testSplitterDir);
         expect(splitterFnResult).toBeNull();
     });
-   
+
+   //test case: testing if input is valid/exists
     test('returns success value', () => {
         const splitterFnResult = splitter(testSplitterDir);
         expect(splitterFnResult).toExist();
     });
- 
+
+    //test case: retuns error bc of invalid input(constIvalidAgenDir)
     test('returns error due to invalid input', () => {
         const splitterFnResult = agent(testInvalidSplitterDir);
         expect(splitterFnResult).toBeTruthy();
    
+   //test case: retuns error bc of falsey input
     test('returns error due to fasley input', () => {
             const splitterFnResult = splitter(testInvalidSplittertDir);
             expect(splitterFnResult).toBeFalsy();
  
     });
- 
+    //test case: returns port splitter is listening on 
     test('listening on port 9997',() => {
         const splitterFnResult = splitter(testSplitterDir);
     expectapp.listen(3000, () => console.log("listening on port ___!"));
